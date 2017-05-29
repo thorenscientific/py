6bee3e5fcd1126d8c202f4b1d46f57bcfe184b8a
Mark Thoren <thorenscientific@gmail.com>
2017-05-29 09:09:10 -0700

    Forgot this one...

---
+++ foundations-of-python-network-programming/05/blocks_big_mark.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# Foundations of Python Network Programming - Chapter 5 - blocks.py
+# Sending data one block at a time.
+
+import socket, struct, sys
+import numpy as np
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+HOST = sys.argv.pop() if len(sys.argv) == 3 else '127.0.0.1'
+PORT = 1060
+struct_obj = struct.Struct('!I')  # for messages up to 2**32 - 1 in length
+
+def recvall(sock, length):
+    data = ''
+    while len(data) < length:
+        more = sock.recv(length - len(data))
+        if not more:
+            raise EOFError('socket closed %d bytes into a %d-byte message'
+                           % (len(data), length))
+        data += more
+    return data
+
+def get(sock):
+    lendata = recvall(sock, struct_obj.size) # Would there be any harm in hard coding lendata to 4?? (portability, etc.)
+
+    (length,) = struct_obj.unpack(lendata)
+    return recvall(sock, length)
+
+def put(sock, message): #Receive data
+    sock.send(struct_obj.pack(len(message)) + message)
+
+if sys.argv[1:] == ['server']:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', PORT)) # Use '' for server to open up to the outside world (network)
+    s.listen(1)
+    print 'size of struct_obj: ' + repr(struct_obj.size)
+    print 'Listening at', s.getsockname()
+    sc, sockname = s.accept()
+    print 'Accepted connection from', sockname
+    sc.shutdown(socket.SHUT_WR)
+    while True:
+        message = get(sc)
+        if not message:
+            break
+        #print 'Message says:', repr(message)
+        print 'testing numpy array stuff'
+        ndata = np.frombuffer(message)
+        print 'size of data: ' + str(len(message))
+        print ndata[:8]
+    sc.close()
+    s.close()
+    print 'going back for another...'
+
+elif sys.argv[1:] == ['client']:
+    s.connect((HOST, PORT))
+    s.shutdown(socket.SHUT_RD)
+    #put(s, 'Beautiful is better than ugly.')
+    #s.send(struct_obj.pack(len('Beautiful is better than ugly.')) + 'Beautiful is better than ugly.')
+    #put(s, 'Explicit is better than implicit.')
+    a = np.random.normal(1.0, 1.0, 1048576)
+    data = bytearray(a)
+    
+    print 'length of message: ' + str(len(data))
+    s.send(struct_obj.pack(len(data)) + data)
+    #put(s, 'Simple is better than complex.')
+    put(s, '')
+    s.close()
+
+else:
+    print >>sys.stderr, 'usage: streamer.py server|client [host]'

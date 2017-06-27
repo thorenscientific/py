c1c6cf65ef951d4368aba37d5185c7ce64243533
gargondoid <gargondoid@gmail.com>
2017-06-26 19:20:42 -0700

    Adding remote version so we can play across the lab

---
+++ Learn_to_Program_with_Minecraft_Code/chapter2-variables/teleport_remote.py
@@ -0,0 +1,11 @@
+#Connect to Minecraft
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create("10.26.148.143")
+
+#Set x, y, and z variables to represent coordinates
+x = 10
+y = 110
+z = 12
+
+#Change the player's position
+mc.player.setTilePos(x, y, z)

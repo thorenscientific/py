9adfcdf97002adf44f404dd7b4aa27237a3e9ce3
Mark Thoren <thorenscientific@gmail.com>
2017-06-27 18:47:03 -0700

    A bit more cleanup, work on python challenge stuff.

--- Python_Challenge/ocr.py
+++ Python_Challenge/ocr.py
@@ -3,8 +3,30 @@
 Created on Sat Nov 05 16:30:05 2016
 
 @author: mthoren
+
+URL summary:
+Level 00 http://www.pythonchallenge.com/pc/def/0.html
+Level 01 http://www.pythonchallenge.com/pc/def/map.html
+Level 02 http://www.pythonchallenge.com/pc/def/ocr.html
+Level 03 http://www.pythonchallenge.com/pc/def/equality.html
+
+Level 05 http://www.pythonchallenge.com/pc/def/peak.html
+Level 06 http://www.pythonchallenge.com/pc/def/channel.html
+
+
+
+
+
 """
 print("Here we go!")
+# http://www.pythonchallenge.com/pc/def/0.html
+# Warmup, photo with 2^38 in it. Change URL to:
+# http://www.pythonchallenge.com/pc/def/274877906944.html
+# brings us to:
+# http://www.pythonchallenge.com/pc/def/map.html
+# Photo of a piece of paper, K -> M, O -> Q, E -> G
+# which is a hint to map each letter to two letters ahead...
+
 for line in open("ocr.txt"):
     for ch in line:
         if ch in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
@@ -47,6 +69,7 @@ for row in range(0, rows):
         print("row " + str(row) + "has nonalpha")
         
 # And with that, we get the text "linkedlist", which gives us our next URL!
+# http://www.pythonchallenge.com/pc/def/linkedlist.html
 
 import urllib
 RunLinkedList = False
@@ -61,6 +84,12 @@ if RunLinkedList == True:
     
 # Ha! This gives us "Yes. Divide by two and keep going." at one point, 
 # and finally, somewhere along the line, "peak.html" which leads to the next challenge!
+# http://www.pythonchallenge.com/pc/def/peak.html
+    
+# Viewing source, gives us a hint: <peakhell src="banner.p"/> so we go to...
+# http://www.pythonchallenge.com/pc/def/banner.p
+# which is a bunch of gibberish ending with a period, so it must be a pickle...
+    
 import pickle
 
 f = urllib.urlopen("http://www.pythonchallenge.com/pc/def/banner.p")
@@ -68,5 +97,35 @@ mypickle = f.read()
 answer = pickle.loads(mypickle)
 print("Pickle answer...")
 print answer
+print("Yuck, let's print element by element to see if we can detect a pattern...")
+for i in range(0,len(answer)):
+    print(answer[i])
+print("Done! Well, nothing obvious yet... how about the number of elements?:")
+for i in range(0,len(answer)):
+    print(len(answer[i]))
+
+# A-ha! I think i see what's going on. Print the character the number of times
+# indicated...
+for i in range(0,len(answer)):
+    mystr = ""
+    for j in range(0, len(answer[i])):
+        for numch in range(0, answer[i][j][1]):
+            mystr += answer[i][j][0]
+    print(mystr)
+# WooHoo! It prints out "channel"!
+# Let's go to...
+# http://www.pythonchallenge.com/pc/def/channel.html
+# Nice pic of a zipper, PayPal donation buttion... let's get going!
+# What the heck, let's go here:
+# http://www.pythonchallenge.com/pc/def/zipper.html
+# Which gives us this hint (and no further hints in source):
+# [:3] todo: tell everyone that [:n] is to take the first n characters...
+# http://www.pythonchallenge.com/pc/def/zip.html
+# tells us "yes, find the zip."  
+    
+    
+    
+
+
 
 
